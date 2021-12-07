dc919287a94166d1de0edb739507cbf415964abb
Sam Place <samvauclain@gmail.com>
2021-12-06 23:36:46 -0800

    works but seems ineffecient

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const Employee = require('./lib/Employee');
 
 function createEmployee() {
     inquirer
-    .prompt({
+    .prompt([
+    {
         type: 'text',
         name: 'name',
         message: 'What is your name?'
-    })
-    .then(({ name }) => {
-        this.employee = new Employee(name);
+    },
+    {
+        type: 'text',
+        name: 'teamManager',
+        message: 'Who is your team manager?'
+    },
+    {
+        type: 'text',
+        name: 'employeeID',
+        message: 'Who is your team manager?'
+    },
+    ])
+    .then(({ name, teamManager, employeeID }) => {
+        this.employee = new Employee(name, teamManager, employeeID);
         console.log(this.employee);
     })
 };
