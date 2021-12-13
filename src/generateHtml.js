5469e4730508b8acf2c457157742183429d5cc36
Sam Place <samvauclain@gmail.com>
2021-12-12 20:52:51 -0800

    trying to get for loop += to work

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,6 +1,13 @@
-function buildCards() {
+
+function generateHtml(data) {
+  console.log('data in generate:', data)
+  console.log('data length in generate:', data.length)
+  var card = '';
+
+  function buildCards() {
+    
     for (var i = 0; i <= data.length; i++) {
-      return `<div class="col">
+      return card += `<div class="col">
       <div class="card" style="width: 18rem;">
         <!-- <img src="..." class="card-img-top" alt="..."> -->
         <div class="card-body bg-primary text-light">
@@ -16,38 +23,34 @@ function buildCards() {
       </div>
       </div>`  
   }
-}
-
-
-function generateHtml(data) {
-console.log('data in generate:', data)
-
-var github = '';
-var officeNum = '';
-var internSchool = '';
-
-return `
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Team Profile Generator</title>
-  </head>
-  <body>
-  <div class="bg-danger text-center text-light py-4">
-  <h1>My Team</h1>
-</div>
-
-<div class="container mt-5">
-    ${buildCards()}
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-  </body>
-</html>
-`
-}
-    
-module.exports = generateHtml;
+  }
+  
+  var github = '';
+  var officeNum = '';
+  var internSchool = '';
+  
+  return `
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <title>Team Profile Generator</title>
+    </head>
+    <body>
+    <div class="bg-danger text-center text-light py-4">
+    <h1>My Team</h1>
+  </div>
+  
+  <div class="container mt-5">
+      ${buildCards()}
+  </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    </body>
+  </html>
+  `
+  }
+      
+  module.exports = generateHtml;
