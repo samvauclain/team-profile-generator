15ca5bceb94ca9c4156a52651078e001943b795d
Sam Place <samvauclain@gmail.com>
2021-12-12 20:35:51 -0800

    added function for for loop

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,3 +1,24 @@
+function buildCards() {
+    for (var i = 0; i <= data.length; i++) {
+      return `<div class="col">
+      <div class="card" style="width: 18rem;">
+        <!-- <img src="..." class="card-img-top" alt="..."> -->
+        <div class="card-body bg-primary text-light">
+          <h5 class="card-title">${data[i].name}</h5>
+          <p class="card-text"></p>
+        </div>
+        <div class="bg-light">
+        <ul class="list-group my-4 px-2">
+          <li class="list-group-item">Employee ID: ${data[i].employeeID}</li>
+          <li class="list-group-item">Email: <a href="mailto:${data[i].email}">${data[i].email}</a></li>  
+          </ul>
+        </div>
+      </div>
+      </div>`  
+  }
+}
+
+
 function generateHtml(data) {
 console.log('data in generate:', data)
 
@@ -20,27 +41,9 @@ return `
   <h1>My Team</h1>
 </div>
 
-<div class="container mt-5">`
-
-  for (var i = 0; i <= data.length; i++) {
-        return `<div class="col">
-        <div class="card" style="width: 18rem;">
-          <!-- <img src="..." class="card-img-top" alt="..."> -->
-          <div class="card-body bg-primary text-light">
-            <h5 class="card-title">${data[i].name}</h5>
-            <p class="card-text"></p>
-          </div>
-          <div class="bg-light">
-          <ul class="list-group my-4 px-2">
-            <li class="list-group-item">Employee ID: ${data[i].employeeID}</li>
-            <li class="list-group-item">Email: <a href="mailto:${data[i].email}">${data[i].email}</a></li>  
-            </ul>
-          </div>
-        </div>
-        </div>`  
-  }
-  
-`</div>
+<div class="container mt-5">
+    ${buildCards()}
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
   </body>
 </html>
